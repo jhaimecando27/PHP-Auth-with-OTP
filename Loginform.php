4a94dc0547eccb8b0ad8fa3f2821cffecfffc148
Jhaime Cando <74945153+jhaimecando27@users.noreply.github.com>
2022-11-07 12:19:02 +0800

    Style: Remove all styles and reformat
    - Hindi mabasa yung code ng maayos dapat properly formatted
    - Tinanggal ko lng ng styles, nagkagulogulo ee. Maganda kung nasa isang
      file sila
    - Yung message nilipat ko sa taas, dun nmn nmn lumabas mga message ee
    - Pinalitan ko yung username ng email, email & password lng nmn
      kailangan
    - Tinaggal ko yung mga may `$_SESSION` sa mga input, hindi nmn need ee,
      ibalik nlng kung kailangan tlga.
    - Yung mga label nilagyan ko ng `for`, tinuturo kasi nun kung para saan
      sila

--- Loginform.php
+++ Loginform.php
@@ -1,35 +1,33 @@
-<?php
-	session_start();
-?>
 <!DOCTYPE html>
 <html>
 <head>
-<title> Login </title>
+    <title>Login</title>
 </head>
+
 <body>
-	        <style>
-            body {
-                background-image: url('Plm.jpg');
-                background-size: 1500px;
-            }
-</style>
-	<h2 style="color: white; text-align: center; font-size: 50px;">Login Form</h2>
-	<form method="POST" action="try.php">
-		<center><div style="color: white;">
-	<label style="color: white; font-size: 30px;">Username:<br></label> <input style="width: 200px; height: 30px;" type="text" value="<?php if (isset($_SESSION["userName"])){echo $_SESSION["user"];}?>" name="username"><br>
-	<label style="color: white; font-size: 30px;">Password:<br></label> <input style="width: 200px; height: 30px;" type="password" value="<?php if (isset($_SESSION["passWord"])){echo $_SESSION["pass"];}?>" name="password"><br><br>
-	<input style="color: white; font-size: 15px;" type="checkbox" name="remember"> Remember me  <br><br>
-	<input style="width: 100px; height: 30px;" type="submit" value="Login" name="login">
-</div></center>
-	</form>
- 
-	<span>
-	<?php
-		if (isset($_SESSION['message'])){
-			echo $_SESSION['message'];
-		}
-		unset($_SESSION['message']);
-	?>
-</span>
+    <?php
+        if (isset($_SESSION['message'])) {
+            echo $_SESSION['message'];
+        }
+        unset($_SESSION['message']);
+    ?>
+
+    <h2>Login Form</h2>
+    <form method="POST" action="">
+        <center>
+            <div>
+                <label for="inputEmail">Email:</label> 
+                <input id="inputEmail" type="text" name="email">
+
+                <label for="inputPassword">Password:</label> 
+                <input id="inputPassword" type="password" name="password">
+
+                <input id="remember" type="checkbox" name="remember">
+                <label for="remember">Remeber me</label> 
+
+                <input type="submit" value="login" name="login">
+            </div>
+        </center>
+    </form>
 </body>
 </html>
