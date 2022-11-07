632ed1301f5ee42659d4661c36d4c51c5fefd49b
Jhaime Cando <74945153+jhaimecando27@users.noreply.github.com>
2022-11-07 15:15:03 +0800

    Feat: Add registration script
    Maiitindihan mo nmn na to siguro may mga comments nmn tas maiitindihan
    mo agad meaning ng function sa name palang

--- Registration.php
+++ Registration.php
@@ -1,3 +1,38 @@
+<?php session_start();
+
+    /* User summitted a form */
+	if(isset($_POST['register'])){
+ 
+        /* Assign sumitted email and password */
+		$email = trim($_POST['email']);
+		$password = trim($_POST['password']);
+
+        /* Add key to each data */
+        $newData = array(
+            "email" => $email,
+            "password" => $password
+        );
+
+        /* Open json file */
+        $json = file_get_contents("Account.json");
+
+        /* Add new data */
+        $tempArray = json_decode($json);
+        array_push($tempArray, $newData);
+        $jsonData = json_encode($tempArray);
+
+        /* Update json file */
+        file_put_contents("Account.json", $jsonData);
+
+        /* Remember the new user */
+        $_SESSION['user_email'] = $email;
+
+        /* Go to index.php */
+        header('location:index.php');
+        exit();
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -23,7 +58,7 @@
                 <label for="inputPassword">Password</label>
                 <input id="inputPassword" type="password" name="password">
 
-                <input type="submit" name="create" value="Sign Up">
+                <input type="submit" name="register" value="Sign Up">
                 
                 <a href="Loginform.php">Login Form</a>
             </div>
